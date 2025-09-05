3b753998c8763100ecc358773ec47cce280956b1
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2025-09-05 12:45:10 -0400

    Update city_reader.py
    Jackie Arteaga

--- city_reader.py
+++ city_reader.py
@@ -1,12 +1,24 @@
 # city_reader.py
 # Read the city data from a CSV File
 # Starter Code from CSC 210
-# Modified by: [Your Name]
+# Modified by: [Jackie Arteaga]
 
 from csv import DictReader
 from city_data import CityData
 
 def read_city_from_csv(file_path, city_name):
-    # YOUR CODE HERE
-    # Read the data for the specific city named 
-    # city_name into a CityData object and return it
+    with open(file_path, 'r') as file:
+        reader = DictReader(file, delimiter='t') # used chatgpt to help for 't' to handle tab-separated data
+        years = []
+        populations = []
+
+        for row in reader:
+            if row ['City'] == city_name:
+                year = int(row['Year']) # help from Google AI to figure out hwo to convert string to integer
+                pop = int(row['Population'])
+                years.append(year)
+                populations.append)pop)
+
+        start_year = min(years)
+        end_year = max(years)
+        return CityData(city_name, start_year, end_year, populations)
