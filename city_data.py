9cd419a2b37537a5a4e92ff50fefe400da57ea4c
Jackie Arteaga <guadalupe.arteaga001@albright.edu>
2025-09-05 12:44:38 -0400

    Update city_data.py
    Jackie Arteaga

--- city_data.py
+++ city_data.py
@@ -1,18 +1,29 @@
 # city_data.py
 # A class for storing a city's population data
 # Starter Code from CSC 210
-# Modified by: [Your Name]
+# Modified by: [Jackie Arteaga]
 
 class CityData:
-    # YOUR CODE HERE
-    # Reverse engineer what is expected here
-    # based on main.py, and tests.py
-    # a couple methods/properties are already done for you;
-    # don't forget to make your constructor and to;
+    def _init_ (self, name, start_year, end_year, data):
+    # private variables
+        self._name = name
+        self._start_year = start_year
+        slef._end_year = end_year
+        self._data + data 
+        # population # by year
+   
 
     @property
     def name(self):
         return self._name
+
+    @property
+    def start_year(self):
+        return self._start_year
+
+    @property
+    def end_year(self):
+        return self._end_year
 
     def __getitem__(self, year):
         if not isinstance(year, int):
@@ -20,4 +31,24 @@ class CityData:
         if year < self._start_year or year > self._end_year:
             raise IndexError("Year out of range")
         return self._data[year - self._start_year]
+
+    @property
+    def min_year(self):
+        min_index = self._data.index(min(self._data))
+        return self._start_year + min_index
+        # what is the lowest population
+
+    @property
+    def max_year(self):
+        max_index = self._data.index(max(self.data))
+        return self._start_year + max_index
+        # what is the highest population
+    # help from chatgpt to find years with max and min population
+
+    def population_growth(self, year1, year2):
+        pop1 = self[year1]
+        pop2 = self[year2]
+        return pop2 - pop1
+        # returns the difference between year2 and year1
+        # help from chatgpt to figure out how to return the difference
     
